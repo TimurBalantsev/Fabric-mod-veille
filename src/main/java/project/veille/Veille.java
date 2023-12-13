201ccc656211d10281ba2c6270208ea47f932600
Timur Balantsev <tbalantsev@gmail.com>
2023-12-12 21:18:10 -0500

    ajout de recipe pour cross et creative grouping

--- src/main/java/project/veille/Veille.java
+++ src/main/java/project/veille/Veille.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import project.veille.item.ModItemGroups;
 import project.veille.item.ModItems;
 
 public class Veille implements ModInitializer {
@@ -12,6 +13,7 @@ public class Veille implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/project/veille/item/ModItemGroups.java
@@ -0,0 +1,28 @@
+package project.veille.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import project.veille.Veille;
+
+public class ModItemGroups {
+    public static final ItemGroup VEILLE_GROUP = Registry.register(
+            Registries.ITEM_GROUP,
+            new Identifier(Veille.MOD_ID, "veille"),
+            FabricItemGroup
+                    .builder()
+                    .displayName(Text.translatable("itemgroup.veille"))
+                    .icon(() -> new ItemStack(ModItems.CROSS)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.CROSS);
+                    })
+                    .build()
+    );
+
+    public static void registerItemGroups() {
+        Veille.LOGGER.info("Registering item groups for" + Veille.MOD_ID);
+    }
+}
