f1b2848f3c7646f0f0c2ba5daf322cf7a6157dba
Timur Balantsev <tbalantsev@gmail.com>
2023-12-12 20:28:55 -0500

    ajout de fichier initiale

---
+++ src/main/java/project/veille/VeilleClient.java
@@ -0,0 +1,10 @@
+package project.veille;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VeilleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
